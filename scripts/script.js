d5a73fbce3e1d07af9b8de88a182f83423ee8aaa
AnoekB <70523341+AnoekB@users.noreply.github.com>
2020-10-24 20:12:01 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,24 @@
 // JavaScript Document
 var hamburgerKnop = document.querySelector("#hamburgerknop");
 var menuZelf = document.querySelector(".hamburgermenu");
+var extraKnop = document.querySelector(".fetuccini");
 
 function menuUitKlappen() {
 	menuZelf.classList.toggle("toonMenu");
+	hamburgerKnop.classList.toggle("kruisMenu");
 }
 
 hamburgerKnop.addEventListener("click", menuUitKlappen);
+
+
+
+var plusKnop = document.querySelector(".plusButton");
+var fetucciniKnop = document.querySelector(".fetuccini");
+
+function optieLatenZien() {
+	fetucciniKnop.classList.toggle("toonOptie");
+	plusKnop.classList.toggle("minMenu");
+
+}
+
+plusKnop.addEventListener("click", optieLatenZien);
